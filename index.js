baba71edf47665a61cf2058c489483d8d3d29945
Adam-IT <Alan_Salons@mail.ru>
2023-05-22 17:16:37 +0300

    модулем OS

---
+++ index.js
@@ -0,0 +1,6 @@
+import os from "os";
+import osp from "./welcome.js";
+
+console.log(os.homedir(), os.hostname(), os.version(), os.type(),);
+
+console.log(osp())
---
+++ welcome.js
@@ -0,0 +1,4 @@
+import os from "os";
+export default function osp (){
+    return `Привет, юзер. Вижу ты зашел с ${os.type}`
+}
